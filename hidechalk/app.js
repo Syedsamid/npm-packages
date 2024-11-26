cb383ab9d78fe81664c0d739192473bc04c73e56
Syedsamid <samidsyed1720@gmail.com>
2024-11-26 07:27:45 +0000

    chalk

---
+++ hidechalk/app.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk'; //* es6
+console.log(chalk.bgBlack("Hello"));
+
+console.log(chalk.red.bgBlue("                                     "));
+console.log(chalk.bgWhite("              🏳️‍⚧️                     "));
+console.log(chalk.bgBlue("                                     "));
